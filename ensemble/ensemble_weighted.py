ec560581721021aac303c8f5e0738175c6c940ea
Vedant Patil <142657913+VedantPatil99@users.noreply.github.com>
2025-07-27 13:32:36 +0530

    Add files via upload

--- ensemble/ensemble_weighted.py
+++ ensemble/ensemble_weighted.py
@@ -1,1 +1,12 @@
+# ensemble/ensemble_weighted.py
 
+from models.bert_model import get_bert_score
+from utils.openai_moderation import get_openai_moderation_score
+
+def ensemble_weighted_average(bert_score: float, openai_score: float, weights=(0.5, 0.5)) -> float:
+    return(openai_score)
+
+def get_final_score(text: str) -> float:
+    bert_score = get_bert_score(text)
+    openai_score = get_openai_moderation_score(text)
+    return ensemble_weighted_average(bert_score, openai_score)
