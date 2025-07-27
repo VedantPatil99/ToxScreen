ab4b28882a110d013553a6dae0bf21c55e888bf6
Vedant Patil <142657913+VedantPatil99@users.noreply.github.com>
2025-07-27 13:28:55 +0530

    Add files via upload

--- api/app.py
+++ api/app.py
@@ -1,1 +1,28 @@
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import StreamingResponse
+from pydantic import BaseModel
+import pandas as pd
+from io import BytesIO
+from utils.perspective import analyze_single_text, analyze_texts
+from utils.report_generator import generate_report_zip
 
+app = FastAPI()
+
+class TextPayload(BaseModel):
+    text: str
+
+@app.post("/analyze-text/")
+def analyze_text(payload: TextPayload):
+    return analyze_single_text(payload.text)
+
+@app.post("/analyze-excel/")
+async def analyze_excel(file: UploadFile = File(...)):
+    input_bytes = await file.read()
+    df = pd.read_excel(BytesIO(input_bytes))
+
+    result_df = analyze_texts(df)
+    zip_bytes = generate_report_zip(result_df)
+
+    return StreamingResponse(BytesIO(zip_bytes), media_type="application/zip", headers={
+        "Content-Disposition": "attachment; filename=toxicity_output.zip"
+    })
